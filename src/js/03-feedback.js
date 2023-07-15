2bb69d189b8b8b24d0adfaf1145d37a45086573c
Anna-1987 <mag260910@gmail.com>
2023-07-15 22:45:26 +0300

    home-8-feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,43 +1,46 @@
 import  throttle  from 'lodash.throttle';
 
+    
 const formEl = document.querySelector('.feedback-form');
-// const inputEl = document.querySelector('.feedback-form input');
-// const textareaEl = document.querySelector('.feedback-form textarea');
-//STORAGE_KEY = 'feedback-form-state';
-const formData = {};
+const LOCAL_STORAGE_KEY = 'feedback-form-state';
 
-
-formEl.addEventListener('submit', onFormSubmit);
-formEl.addEventListener('input', throttle(onFormInput, 500));
+let formData = {};
 
 populateTextOutput();
 
-function onFormSubmit(event){
-    event.preventDefault();
-    console.log('forma');
+formEl.addEventListener('input', throttle(onFormInput, 500));
 
-    event.currentTarget.reset();
-    localStorage.removeItem('feedback-form-state');
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormInput(event) {
+  formData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || {};
+
+  formData[event.target.name] = event.target.value;
+
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
 }
 
-function onFormInput(event){
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (!event.target.email.value || !event.target.message.value) {
+    alert('Enter all data');
+    return;
+  }
 
-     formData[event.target.name] = event.target.value;
-    console.log(formData);
-
-   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-   
+  event.target.reset();
+  console.log(formData);
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
 }
-    function populateTextOutput(){
-        const saveData = JSON.parse(localStorage.getItem('feedback-form-state'));
-        const email = document.querySelector('.feedback-form input');
-        const message = document.querySelector('.feedback-form textarea');
-        if (saveData) {
-          email.value = saveData.email;
-          message.value = saveData.message;
-        }
-           
-     }
-    
+
+function populateTextOutput() {
+    let saveData = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (!saveData) {
+      return;
+    }
+
+    formData = saveData;
+    formEl.email.value = formData.email || '';
+    formEl.message.value = formData.message || '';
+}
 
 
