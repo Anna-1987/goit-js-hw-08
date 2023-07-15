421f0929c1844647c8320dc72dec42a2d62db4ad
Anna-1987 <mag260910@gmail.com>
2023-07-15 16:44:49 +0300

    home-8-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,18 @@
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe); 
 
-player.on('timeupdate', throttle(onPlay, 1000));
 
-function onPlay({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
-}
+const getCurrentTime = function (currentTime) {
+  const seconds = currentTime.seconds;
+  localStorage.setItem(CURRENT_TIME_KEY, JSON.stringify(seconds));
+};
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME_KEY)) || 0);
