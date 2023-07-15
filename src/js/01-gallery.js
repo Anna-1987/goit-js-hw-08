869e3e103f6ac501dc735dab098f2c63a827726a
Anna-1987 <mag260910@gmail.com>
2023-07-15 15:29:57 +0300

    home-8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const galleryMarkup = galleryItems.map((image) => {
+    return `<li class = "gallery__item"><a href="${image.original}" class = "gallery__link">
+          <img class = "gallery__image" data-index="${image.original}" src="${image.preview}" alt="${image.description}"
+    </a></li>`;
+  }).join('');
+  
+  const galleryList = document.querySelector(".gallery");
+  galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
+console.log(lightbox);
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import  throttle  from 'lodash.throttle';
+
+const formEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('.feedback-form input');
+const textareaEl = document.querySelector('.feedback-form textarea');
+STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+populateTextOutput();
+
+function onFormSubmit(event){
+    event.preventDefault();
+    console.log('forma');
+
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput(event){
+
+     formData[event.target.name] = event.target.value;
+    console.log(formData);
+
+   const str = localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+   console.log(str);
+
+   
+}
+    function populateTextOutput(){
+        const saveData = JSON.parse(localStorage.getItem('feedback-form-state'));
+        const email = document.querySelector('.feedback-form input');
+        const message = document.querySelector('.feedback-form textarea');
+        if (saveData) {
+          email.value = saveData.email;
+          message.value = saveData.message;
+        }
+           
+     }
+    
+
 
